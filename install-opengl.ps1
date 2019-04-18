806ab6ca9a07ebb67e2c6f180cc851112e952442
jvbsl <jvsbl@gmx.de>
2019-04-19 00:37:33 +0200

    icacls

--- install-opengl.ps1
+++ install-opengl.ps1
@@ -21,7 +21,7 @@ function DownloadMesaOpenGL ($architecture) {
     if (Test-Path $filepath) {
         Write-Host "OpenGL already installed"
         Clear-History
-        cacls "$filepath" /C /G "$env:UserName":F
+        Icacls "$filepath" /grant "$env:UserName":F
         Remove-Item $filepath -Force
         Get-History
     }
